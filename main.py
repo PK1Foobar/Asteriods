2bcc6c8205d74d21ad144bb744476c58c3485aa5
PK_Foobar <kampfhenkeljacob7@gmail.com>
2025-12-05 15:16:55 +0100

    Refactor main.py to use direct imports for SCREEN_WIDTH and SCREEN_HEIGHT and added a FPS and tick display

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 pygame.init()
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: ",pygame.version.ver)
-    print (f"Screen width: {constants.SCREEN_WIDTH}")
-    print (f"Screen height: {constants.SCREEN_HEIGHT}")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +21,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+        print(dt)
+
         
 if __name__ == "__main__":
     main()
