6ba941cb8a4146cdfa86e88bedf9c10ff3e102a5
PK_Foobar <kampfhenkeljacob7@gmail.com>
2025-12-05 14:56:10 +0100

    Initialize Asteroids project with main functionality, constants, logging, and configuration files

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
-import constants.py
+import constants
+from logger import log_state
+pygame.init()
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 def main():
     print("Starting Asteroids with pygame version: ",pygame.version.ver)
-    print(constants.py)
-
+    print (f"Screen width: {constants.SCREEN_WIDTH}")
+    print (f"Screen height: {constants.SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
