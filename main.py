7baaaa1a78a543fff5e2182aebb5c8476db1f81e
PK_Foobar <kampfhenkeljacob7@gmail.com>
2025-12-05 14:20:01 +0100

    add main

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import constants.py
+def main():
+    print("Starting Asteroids with pygame version: ",pygame.version.ver)
+    print(constants.py)
+
+if __name__ == "__main__":
+    main()
